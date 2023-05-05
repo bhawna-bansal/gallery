de49c989f5277727d9a857a82dc1b1111d4b8b0f
bhawna-bansal <bhawna.bansal@bluebash.co>
2023-05-05 15:16:38 +0530

    video_gallery

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ApplicationMailer
-  # default from: 'bansalbhawna30@gmail.com'
-  # def welcome_email(user)
-  #   @user = user
-  #   @url  = 'http://localhost:3000/users/sign_up'
-  #   mail(to: @user.email,subject: 'welcome to my awesome site')
-  # end
+  default from: 'bansalbhawna30@gmail.com'
+  def welcome_send(user)
+    @user = user
+    @url  = 'http://localhost:3000/users/sign_up'
+    mail(to: @user.email,subject: 'welcome to my awesome site')
+  end
 end
